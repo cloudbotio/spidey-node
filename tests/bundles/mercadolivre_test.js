41ca0154a3039dc025601d4e2d8839c8c9ab432f
spitfire2903 <nm.ricardo@gmail.com>
2013-10-01 21:29:28 -0300

    Mercadolivre bundle

---
+++ tests/bundles/mercadolivre_test.js
@@ -0,0 +1,16 @@
+var Bundle = require("../../worker/bundles/bundle");
+
+exports.test_simpleMercadolivre = function(test){
+
+    var bundle = new Bundle("ecommerce/mercadolivre", {
+		q: "smartphone"
+	});
+
+	test.expect(2)
+ 	test.ok(bundle, "The bundle shoud be valid");
+
+	bundle.getItems(function(items){
+		test.ok(items, "The items shoud be valid");
+		test.done();
+	})
+}
